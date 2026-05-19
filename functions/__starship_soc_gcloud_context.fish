fc06d020b97fb39ccb135665bce0b6f2c4e26dd3
chelokot <andrey.vlasenko.work@gmail.com>
2026-05-19 22:01:19 +0300

    Avoid CLI calls in context providers

--- functions/__starship_soc_gcloud_context.fish
+++ functions/__starship_soc_gcloud_context.fish
@@ -1,14 +1,46 @@
 function __starship_soc_gcloud_context
     set -l account
     set -l project
+    set -l config_home
 
-    if command -q gcloud
-        if command -q timeout
-            set account (command timeout 0.2s gcloud config get-value account 2>/dev/null)
-            set project (command timeout 0.2s gcloud config get-value project 2>/dev/null)
-        else
-            set account (command gcloud config get-value account 2>/dev/null)
-            set project (command gcloud config get-value project 2>/dev/null)
+    if set -q CLOUDSDK_CONFIG
+        set config_home $CLOUDSDK_CONFIG
+    else
+        set config_home "$HOME/.config/gcloud"
+    end
+
+    set -l active_config default
+    if test -r "$config_home/active_config"
+        set active_config (string trim -- (cat "$config_home/active_config"))
+    end
+
+    set -l config_file "$config_home/configurations/config_$active_config"
+    if test -r "$config_file"
+        set -l parsed (awk -F= '
+            function trim(value) {
+                gsub(/^[ \t]+|[ \t]+$/, "", value)
+                return value
+            }
+            /^\[/ {
+                section = $0
+                gsub(/^\[|\]$/, "", section)
+                next
+            }
+            section == "core" {
+                key = trim($1)
+                value = trim($2)
+                if (key == "account") account = value
+                if (key == "project") project = value
+            }
+            END {
+                print account
+                print project
+            }
+        ' "$config_file")
+
+        set account $parsed[1]
+        if test (count $parsed) -gt 1
+            set project $parsed[2]
         end
     end
 
--- functions/__starship_soc_kube_context.fish
+++ functions/__starship_soc_kube_context.fish
@@ -1,14 +1,63 @@
 function __starship_soc_kube_context
     set -l context
     set -l namespace
+    set -l kubeconfig_files
 
-    if command -q kubectl
-        if command -q timeout
-            set context (command timeout 0.2s kubectl config current-context 2>/dev/null)
-            set namespace (command timeout 0.2s kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)
-        else
-            set context (command kubectl config current-context 2>/dev/null)
-            set namespace (command kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)
+    if set -q KUBECONFIG
+        set kubeconfig_files (string split : -- $KUBECONFIG)
+    else
+        set kubeconfig_files "$HOME/.kube/config"
+    end
+
+    set -l readable_files
+    for file in $kubeconfig_files
+        if test -r "$file"
+            set -a readable_files "$file"
+        end
+    end
+
+    if test (count $readable_files) -gt 0
+        set -l parsed (awk '
+            function trim(value) {
+                gsub(/^[ \t]+|[ \t]+$/, "", value)
+                return value
+            }
+            /^current-context:[ \t]*/ {
+                current = trim(substr($0, index($0, ":") + 1))
+                next
+            }
+            /^contexts:[ \t]*$/ {
+                in_contexts = 1
+                next
+            }
+            /^[A-Za-z0-9_-]+:[ \t]*$/ && $0 !~ /^contexts:/ {
+                in_contexts = 0
+                in_item = 0
+            }
+            in_contexts && /^- context:[ \t]*$/ {
+                in_item = 1
+                namespace = ""
+                next
+            }
+            in_contexts && in_item && /^[ \t]+namespace:[ \t]*/ {
+                namespace = trim(substr($0, index($0, ":") + 1))
+                next
+            }
+            in_contexts && in_item && /^[ \t]+name:[ \t]*/ {
+                name = trim(substr($0, index($0, ":") + 1))
+                namespaces[name] = namespace
+                in_item = 0
+                next
+            }
+            END {
+                print current
+                print namespaces[current]
+            }
+        ' $readable_files)
+
+        set context $parsed[1]
+        if test (count $parsed) -gt 1
+            set namespace $parsed[2]
         end
     end
 
